953072cba76a34aa33e2ead20cc78092be181ba9
MuhammadZikri <zikrim577@gmail.com>
2022-04-30 10:51:25 +0700

    boolean

---
+++ boolean.js
@@ -0,0 +1,5 @@
+document.writeln("<br>");
+document.writeln("<li> Data boolean dengan output file secara terpisah : </li>");
+document.writeln(true);
+document.writeln("<br>");
+document.writeln(false);
